d1a5b3e93eea4bfa0daf47977e02b166db9ef2e7
Travisbui <122054086+Travisbui@users.noreply.github.com>
2023-02-21 11:01:02 -0800

    Prototype Challenge 03, 5 ongoing

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,14 +7,14 @@ public class SpawnManager : MonoBehaviour
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
     private float startDelay = 2;
-    private float repeatRate = 2;
+    private float spawnRepeating = 2;
     private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
-        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        InvokeRepeating ("SpawnObjects", startDelay, spawnRepeating);
     }
 
     // Update is called once per frame
